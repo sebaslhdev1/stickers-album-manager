c1f62612301e8236f697c07b6fedaf340b99f2cb
Sebastian Loaiza <sebastian.loaiza@nousware.ai>
2026-05-04 14:41:47 -0500

    feat: resize apple icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "KardKeeper",
   description: "Manage your albums collection",
-  icons: {
-    apple: "/icons/icon-192.png",
-  },
 }
 
 export default function RootLayout({
